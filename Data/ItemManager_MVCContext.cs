9ba2cf176ab95515cff9b3f41cf64b2ad28cd8d5
Marcello FABBRI <marcello.fabbri@alten.it>
2022-03-09 12:32:25 +0100

    First Commit test git

---
+++ Data/ItemManager_MVCContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ItemManager_MVC.Models;
+
+namespace ItemManager_MVC.Data
+{
+    public class ItemManager_MVCContext : DbContext
+    {
+        public ItemManager_MVCContext (DbContextOptions<ItemManager_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ItemManager_MVC.Models.Item> Item { get; set; }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItemManager_MVC.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
